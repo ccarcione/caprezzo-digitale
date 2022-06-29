33282bed57dcf6550b4981326d5ed33edb404e10
Carcione Christian <carcione.christian@gmail.com>
2022-06-29 19:01:18 +0200

    remove old angular project

--- public-client/src/app/layout/shell/shell-content.directive.ts
+++
@@ -1,6 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: '[shell-content]'
-})
-export class ShellContentDirective { }
--- public-client/src/app/layout/shell/shell-menu.directive.ts
+++
@@ -1,6 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: '[shell-menu]'
-})
-export class ShellMenuDirective { }
